22efd432736ec486d3d1f3b4401b018fa9fdf2ff
Philipp Wolfer <ph.wolfer@gmail.com>
2020-11-20 09:33:08 +0100

    Extracted listenbrainz_submit from othe projects into own module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='listenbrainz_submit',
+      version='1.0',
+      description='Simple client library to submit listens to the ListenBrainz server',
+      url='http://github.com/phw/python-listenbrainz-submit',
+      author='Philipp Wolfer',
+      author_email='ph.wolfer@gmail.com',
+      license='MIT',
+      packages=['listenbrainz_submit'])
